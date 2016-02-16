ce7f763a1a821a9d5b8ad602c607555be7b9068e
Tomasz Cichoń <mrciastexx@gmail.com>
2016-02-16 14:34:28 +0100

    Added keyboard wrappers.

---
+++ Spectrum.API/Keyboard.cs
@@ -0,0 +1,20 @@
+﻿namespace Spectrum.API
+{
+    public class Keyboard
+    {
+        public static bool IsKeyPressed(string key)
+        {
+            return UnityEngine.Input.GetKey(key);
+        }
+
+        public static bool IsKeyDown(string key)
+        {
+            return UnityEngine.Input.GetKeyDown(key);
+        }
+
+        public static bool IsKeyUp(string key)
+        {
+            return UnityEngine.Input.GetKeyUp(key);
+        }
+    }
+}
