aaed30ca75942bdc8f2793ffd8a0d01ae64109a1
Tomasz Cichoń <mrciastexx@gmail.com>
2016-02-17 11:29:22 +0100

    Changed the way keyboard keys are parsed.

---
+++ Spectrum.API/Input/Keyboard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Spectrum.API.Input
+{
+    public class Keyboard
+    {
+        public static bool IsKeyPressed(string key)
+        {
+            return UnityEngine.Input.GetKey((KeyCode)Enum.Parse(typeof(KeyCode), key));
+        }
+
+        public static bool IsKeyDown(string key)
+        {
+            return UnityEngine.Input.GetKeyDown((KeyCode)Enum.Parse(typeof(KeyCode), key));
+        }
+
+        public static bool IsKeyUp(string key)
+        {
+            return UnityEngine.Input.GetKeyUp((KeyCode)Enum.Parse(typeof(KeyCode), key));
+        }
+    }
+}
