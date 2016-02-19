280c5570ec42c61fb01fb472ba3bd41630d1a3f0
Tomasz Cichoń <mrciastexx@gmail.com>
2016-02-19 11:57:36 +0100

    Distance build is now read/write.

--- Spectrum.API/Version.cs
+++ Spectrum.API/Version.cs
@@ -3,6 +3,11 @@
     public class Version
     {
         public const int APILevel = 1;
-        public static int DistanceBuild => SVNRevision.number_;
+
+        public static int DistanceBuild
+        {
+            get { return SVNRevision.number_; }
+            set { SVNRevision.number_ = value; }
+        }
     }
 }
