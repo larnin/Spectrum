af594f18de02e620cc7a3ec4908d40c45a21bb3d
Tomasz Cichoń <mrciastexx@gmail.com>
2016-02-19 12:27:16 +0100

    Added a few basic audio properties.

--- Spectrum.API/Game/Audio.cs
+++ Spectrum.API/Game/Audio.cs
@@ -1,11 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Spectrum.API.Game
+﻿namespace Spectrum.API.Game
 {
     public static class Audio
     {
+        public static bool CurrentSongLoops => G.Sys.AudioManager_.CurrentSongLoops();
+
+        public static bool HighPassFilterEnabled
+        {
+            set { G.Sys.AudioManager_.SetQuarantineFilter(value); }
+        }
+
+        public static float MusicVolume
+        {
+            get { return G.Sys.AudioManager_.GetMusicVolume(); }
+            set { G.Sys.AudioManager_.SetMusicVolume(value); }
+        }
+
+        public static float VehicleVolume
+        {
+            get { return G.Sys.AudioManager_.GetCarVolume(); }
+            set { G.Sys.AudioManager_.SetCarVolume(value); }
+        }
+
+        public static float AmbientVolume
+        {
+            get { return G.Sys.AudioManager_.GetAmbientVolume(); }
+            set { G.Sys.AudioManager_.SetAmbientVolume(value); }
+        }
+
+        public static float ObstacleVolume
+        {
+            get { return G.Sys.AudioManager_.GetObstacleVolume(); }
+            set { G.Sys.AudioManager_.SetObstacleVolume(value); }
+        }
+
+        public static float MenuVolume
+        {
+            get { return G.Sys.AudioManager_.GetMenuVolume(); }
+            set { G.Sys.AudioManager_.SetMenuVolume(value); }
+        }
     }
 }
