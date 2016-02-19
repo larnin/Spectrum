3658f4bb87f7f62abe184159aa3b424b9f7a9e5b
Tomasz Cichoń <mrciastexx@gmail.com>
2016-02-19 19:34:55 +0100

    Added race-related events.

---
+++ Spectrum.API/Game/Race.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Spectrum.API.Game
+{
+    public class Race
+    {
+        public static event EventHandler Finished;
+        public static event EventHandler Started;
+        public static event EventHandler Loaded;
+
+        static Race()
+        {
+            Events.ServerToClient.ModeFinished.Subscribe(data =>
+            {
+                Finished?.Invoke(default(object), System.EventArgs.Empty);
+            });
+
+            Events.GameMode.Go.Subscribe(data =>
+            {
+                Started?.Invoke(default(object), System.EventArgs.Empty);
+            });
+
+            Events.GameMode.ModeStarted.Subscribe(data =>
+            {
+                Loaded?.Invoke(default(object), System.EventArgs.Empty);
+            });
+        }
+    }
+}
