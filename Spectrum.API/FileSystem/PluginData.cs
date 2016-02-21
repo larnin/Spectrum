527cf4121ea1947b11f5a89bc04ec70b3599aacc
Tomasz Cichoń <mrciastexx@gmail.com>
2016-02-21 10:19:43 +0100

    PluginData now initializes given directory.

--- Spectrum.API/FileSystem/PluginData.cs
+++ Spectrum.API/FileSystem/PluginData.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.IO;
 
 namespace Spectrum.API.FileSystem
 {
     public class PluginData
     {
+        public string DirectoryName { get; }
+        public string DirectoryPath => Path.Combine(Defaults.PluginDataDirectory, DirectoryName);
+
         public PluginData(Type type)
         {
-            
+            DirectoryName = type.Assembly.GetName().Name;
+
+            if (!Directory.Exists(DirectoryPath))
+            {
+                Directory.CreateDirectory(DirectoryPath);
+            }
         }
     }
 }
