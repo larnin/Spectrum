0e0d6b366d1f8e013a5fa247c041e401ded85d4e
Tomasz Cichoń <mrciastexx@gmail.com>
2016-02-22 14:36:54 +0100

    Added MainMenu class

---
+++ Spectrum.API/Game/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Spectrum.API.Game
+{
+    public class MainMenu
+    {
+        public static event EventHandler Loaded;
+
+        static MainMenu()
+        {
+            Events.MainMenu.Initialized.Subscribe(data =>
+            {
+                Loaded?.Invoke(default(object), System.EventArgs.Empty);
+            });
+        }
+    }
+}
