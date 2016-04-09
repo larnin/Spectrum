54aab7424747dc8df85113a0cf7fecfc31b49846
Tomasz Cichoń <mrciastexx@gmail.com>
2016-04-09 12:41:21 +0200

    Chat message events are now more reliable.

--- Spectrum.API/Game/EventArgs/Network/ChatMessageEventArgs.cs
+++ Spectrum.API/Game/EventArgs/Network/ChatMessageEventArgs.cs
@@ -2,10 +2,12 @@ namespace Spectrum.API.Game.EventArgs.Network
 {
     public class ChatMessageEventArgs : System.EventArgs
     {
+        public string Author { get; private set; }
         public string Message { get; private set; }
 
-        public ChatMessageEventArgs(string message)
+        public ChatMessageEventArgs(string author, string message)
         {
+            Author = author;
             Message = message;
         }
     }
--- Spectrum.API/Game/Network/Chat.cs
+++ Spectrum.API/Game/Network/Chat.cs
@@ -12,15 +12,42 @@ namespace Spectrum.API.Game.Network
         {
             Events.Local.ChatSubmitMessage.Subscribe(data =>
             {
-                var eventArgs = new ChatMessageEventArgs(data.message_);
+                var eventArgs = new ChatMessageEventArgs(G.Sys.PlayerManager_.Current_.profile_.Name_, data.message_);
                 MessageSent?.Invoke(null, eventArgs);
             });
 
             Events.ClientToAllClients.ChatMessage.Subscribe(data =>
             {
-                var eventArgs = new ChatMessageEventArgs(data.message_);
-                MessageReceived?.Invoke(null, eventArgs);
+                var author = ExtractMessageAuthor(data.message_);
+
+                if (author != G.Sys.PlayerManager_.Current_.profile_.Name_ && !IsSystemMessage(data.message_))
+                {
+                    var eventArgs = new ChatMessageEventArgs(author, ExtractMessageBody(data.message_));
+                    MessageReceived?.Invoke(null, eventArgs);
+                }
             });
+        }
+
+        private static string ExtractMessageAuthor(string message)
+        {
+            // 1. [xxxxxx]user[xxxxxx]: adfsafasf
+            var withoutFirstColorTag = message.Substring(message.IndexOf(']') + 1, message.Length - message.IndexOf(']') - 1);
+            // 2. user[xxxxxx]: adfsafasf
+            var withoutSecondColorTag = withoutFirstColorTag.Substring(0, withoutFirstColorTag.IndexOf('['));
+            // 3. user
+
+            return withoutSecondColorTag;
+        }
+
+        private static string ExtractMessageBody(string message)
+        {
+            // 1. [xxxxxx]user[xxxxxx]: body
+            return message.Substring(message.IndexOf(':') + 2); // 2 because we need to cut the leading space
+        }
+
+        private static bool IsSystemMessage(string message)
+        {
+            return message.Contains("[c]") && message.Contains("[/c]");
         }
     }
 }
