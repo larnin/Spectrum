1654f848463501c8758b4ec9427006cb09a4ee38
Tomasz Cichoń <mrciastexx@gmail.com>
2016-04-09 10:36:34 +0200

    Removed redundant using directives.

--- Spectrum.API/Game/EventArgs/Network/ServerCreatedEventArgs.cs
+++ Spectrum.API/Game/EventArgs/Network/ServerCreatedEventArgs.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 namespace Spectrum.API.Game.EventArgs.Network
 {
     public class ServerCreatedEventArgs : System.EventArgs
