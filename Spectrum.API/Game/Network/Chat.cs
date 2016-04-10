cd114a8efcf391d78da4b54d3f9e48de1cc6843b
Tomasz Cichoń <mrciastexx@gmail.com>
2016-04-10 14:00:53 +0200

    Added more chat interaction options.

--- Spectrum.API/Game/Network/Chat.cs
+++ Spectrum.API/Game/Network/Chat.cs
@@ -1,4 +1,7 @@
 using System;
+using Events;
+using Events.ClientToAllClients;
+using Events.Local;
 using Spectrum.API.Game.EventArgs.Network;
 
 namespace Spectrum.API.Game.Network
@@ -7,17 +10,20 @@ namespace Spectrum.API.Game.Network
     {
         public static event EventHandler<ChatMessageEventArgs> MessageReceived;
         public static event EventHandler<ChatMessageEventArgs> MessageSent;
+        public static event EventHandler<ChatActionEventArgs> ActionReceived;
 
         static Chat()
         {
-            Events.Local.ChatSubmitMessage.Subscribe(data =>
+            ChatSubmitMessage.Subscribe(data =>
             {
                 var eventArgs = new ChatMessageEventArgs(G.Sys.PlayerManager_.Current_.profile_.Name_, data.message_);
                 MessageSent?.Invoke(null, eventArgs);
             });
 
-            Events.ClientToAllClients.ChatMessage.Subscribe(data =>
+            ChatMessage.Subscribe(data =>
             {
+                Console.WriteLine(data.message_);
+
                 var author = ExtractMessageAuthor(data.message_);
 
                 if (author != G.Sys.PlayerManager_.Current_.profile_.Name_ && !IsSystemMessage(data.message_))
@@ -26,23 +32,67 @@ namespace Spectrum.API.Game.Network
                     MessageReceived?.Invoke(null, eventArgs);
                 }
             });
+
+            ToAllClientsRemotePlayerActionMessage.Subscribe(data =>
+            {
+                if (G.Sys.NetworkingManager_.IsOnline_)
+                {
+                    var nickname = G.Sys.PlayerManager_.PlayerList_[data.index_].Username_;
+                    var eventArgs = new ChatActionEventArgs(data.index_, nickname, data.message_);
+                    ActionReceived?.Invoke(null, eventArgs);
+                }
+            });
+        }
+
+        public static void SendMessage(string message)
+        {
+            StaticEvent<ChatSubmitMessage.Data>.Broadcast(new ChatSubmitMessage.Data(message));
+        }
+
+        public static void SendAction(string actionMessage)
+        {
+            StaticEvent<PlayerActionMessage.Data>.Broadcast(new PlayerActionMessage.Data(" " + actionMessage));
+        }
+
+        public static void AddLocalMessage(string message)
+        {
+            BroadcastAllEvent<ChatMessage.Data>.BroadcastLocal(new ChatMessage.Data(message));
+        }
+
+        public static void ClearLog()
+        {
+            ChatLog.ClearLog();
         }
 
         private static string ExtractMessageAuthor(string message)
         {
-            // 1. [xxxxxx]user[xxxxxx]: adfsafasf
-            var withoutFirstColorTag = message.Substring(message.IndexOf(']') + 1, message.Length - message.IndexOf(']') - 1);
-            // 2. user[xxxxxx]: adfsafasf
-            var withoutSecondColorTag = withoutFirstColorTag.Substring(0, withoutFirstColorTag.IndexOf('['));
-            // 3. user
+            try
+            {
+                // 1. [xxxxxx]user[xxxxxx]: adfsafasf
+                var withoutFirstColorTag = message.Substring(message.IndexOf(']') + 1, message.Length - message.IndexOf(']') - 1);
+                // 2. user[xxxxxx]: adfsafasf
+                var withoutSecondColorTag = withoutFirstColorTag.Substring(0, withoutFirstColorTag.IndexOf('['));
+                // 3. user
 
-            return withoutSecondColorTag;
+                return withoutSecondColorTag;
+            }
+            catch
+            {
+                return string.Empty;
+            }
         }
 
         private static string ExtractMessageBody(string message)
         {
-            // 1. [xxxxxx]user[xxxxxx]: body
-            return message.Substring(message.IndexOf(':') + 1).Trim();
+            try
+            {
+                // 1. [xxxxxx]user[xxxxxx]: body
+                return message.Substring(message.IndexOf(':') + 1).Trim();
+            }
+            catch
+            {
+                return string.Empty;
+            }
         }
 
         private static bool IsSystemMessage(string message)
