4754df0cdc919d1a4f3d958285e4a485bb87aa60
Tomasz Cichoń <mrciastexx@gmail.com>
2016-04-11 20:17:45 +0200

    Added level-related information, plus development mode property.

--- Spectrum.API/Game/Game.cs
+++ Spectrum.API/Game/Game.cs
@@ -1,7 +1,23 @@
+using System.Reflection;
+
 namespace Spectrum.API.Game
 {
     public class Game
     {
         public static GameMode CurrentMode => (GameMode)G.Sys.GameManager_.Mode_.GameModeID_;
+
+        public static bool IsDevelopmentModeActive
+        {
+            get { return G.Sys.GameManager_.IsDevBuild_; }
+            set
+            {
+                var fieldInfo = G.Sys.GameManager_.GetType().GetField("isDevBuild_", BindingFlags.Instance | BindingFlags.NonPublic);
+                fieldInfo?.SetValue(G.Sys.GameManager_, value);
+            }
+        }
+
+        public static string LevelName => G.Sys.GameManager_.LevelName_;
+        public static string LevelPath => G.Sys.GameManager_.LevelPath_;
+        public static string SceneName => G.Sys.GameManager_.SceneName_;
     }
 }
