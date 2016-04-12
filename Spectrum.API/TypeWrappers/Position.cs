f3bb3a987cbd3f5b85d850bb829890caefc06ef7
Tomasz Cichoń <mrciastexx@gmail.com>
2016-04-12 15:19:13 +0200

    Added distance and nearest named object position/distance calculation.

--- Spectrum.API/TypeWrappers/Position.cs
+++ Spectrum.API/TypeWrappers/Position.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
 namespace Spectrum.API.TypeWrappers
 {
     public class Position
@@ -14,5 +19,49 @@ namespace Spectrum.API.TypeWrappers
         }
 
         public Position(float x, float y) : this(x, y, 0) { }
+
+        public float DistanceTo(Position position)
+        {
+            var xPow = Math.Pow(position.X - X, 2);
+            var yPow = Math.Pow(position.Y - Y, 2);
+            var zPow = Math.Pow(position.Z - Z, 2);
+
+            var distance = Math.Sqrt(xPow + yPow + zPow);
+
+            return (float)distance;
+        }
+
+        public static Position GetNearest(Position position, string name)
+        {
+            var gameObjects = Object.FindObjectsOfType<GameObject>();
+            var validGameObjects = new List<GameObject>();
+
+            foreach (var gameObject in gameObjects)
+            {
+                if (gameObject.name == name)
+                    validGameObjects.Add(gameObject);
+            }
+
+            var nearestObjectDistance = float.MaxValue;
+            var nearestObjectPosition = new Position(float.MaxValue, float.MaxValue, float.MaxValue);
+            foreach (var validGameObject in validGameObjects)
+            {
+                var gameObjectPosition = new Position(validGameObject.transform.position.x, validGameObject.transform.position.y, validGameObject.transform.position.z);
+                var distance = position.DistanceTo(gameObjectPosition);
+
+                if (distance < nearestObjectDistance)
+                {
+                    nearestObjectDistance = distance;
+                    nearestObjectPosition = gameObjectPosition;
+                }
+            }
+            return nearestObjectPosition;
+        }
+
+        public static float GetNearestDistance(Position position, string name)
+        {
+            var nearestPosition = GetNearest(position, name);
+            return position.DistanceTo(nearestPosition);
+        }
     }
 }
