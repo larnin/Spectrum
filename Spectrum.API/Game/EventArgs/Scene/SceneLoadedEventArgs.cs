9df3829846ee749aa5d0185d56660a83b732ea6a
Tomasz Cichoń <mrciastexx@gmail.com>
2016-04-24 12:12:54 +0200

    Added scene loaded notifier event.

---
+++ Spectrum.API/Game/EventArgs/Scene/SceneLoadedEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace Spectrum.API.Game.EventArgs.Scene
+{
+    public class SceneLoadedEventArgs : System.EventArgs
+    {
+        public string SceneName { get; }
+
+        public SceneLoadedEventArgs(string sceneName)
+        {
+            SceneName = sceneName;
+        }
+    }
+}
---
+++ Spectrum.API/Game/Scene.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Spectrum.API.Game.EventArgs.Scene;
+
+namespace Spectrum.API.Game
+{
+    public class Scene
+    {
+        public static event EventHandler<SceneLoadedEventArgs> Loaded;
+
+        static Scene()
+        {
+            Events.Scene.LoadFinish.Subscribe(data =>
+            {
+                var eventArgs = new SceneLoadedEventArgs(data.sceneName);
+                Loaded?.Invoke(null, eventArgs);
+            });
+        }
+    }
+}
