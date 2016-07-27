e98eb11840efc947a052f14308c9715c2054840c
Tomasz Cichoń <ciastexx@live.com>
2016-07-27 13:31:36 +0200

    Can remove and optionally overwrite files now.

--- Spectrum.API/FileSystem/PluginData.cs
+++ Spectrum.API/FileSystem/PluginData.cs
@@ -18,9 +18,22 @@ namespace Spectrum.API.FileSystem
             }
         }
 
-        public string CreateFile(string fileName)
+        public string CreateFile(string fileName, bool overwrite = false)
         {
             var targetFilePath = Path.Combine(DirectoryPath, fileName);
+
+            if (File.Exists(targetFilePath))
+            {
+                if (overwrite)
+                {
+                    RemoveFile(fileName);
+                }
+                else
+                {
+                    Console.WriteLine($"API: Couldn't create a PluginData file for path '{targetFilePath}'. The file already exists.");
+                    return string.Empty;
+                }
+            }
 
             try
             {
@@ -31,6 +44,26 @@ namespace Spectrum.API.FileSystem
             {
                 Console.WriteLine($"API: Couldn't create a PluginData file for path {targetFilePath}. Exception below:\n{ex}");
                 return string.Empty;
+            }
+        }
+
+        public void RemoveFile(string fileName)
+        {
+            var targetFilePath = Path.Combine(DirectoryPath, fileName);
+
+            if (!File.Exists(targetFilePath))
+            {
+                Console.WriteLine($"API: Couldn't delete a PluginData file for path '{targetFilePath}'. File does not exist.");
+                return;
+            }
+
+            try
+            {
+                File.Delete(targetFilePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"API: Couldn't delete a PluginData file for path '{targetFilePath}'. Exception below:\n{ex}");
             }
         }
 
@@ -71,7 +104,7 @@ namespace Spectrum.API.FileSystem
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"API: Couldn't create a PluginData directory for path {targetDirectoryPath}. Exception below:\n{ex}");
+                Console.WriteLine($"API: Couldn't create a PluginData directory for path '{targetDirectoryPath}'. Exception below:\n{ex}");
                 return string.Empty;
             }
         }
