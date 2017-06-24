ccd0d171293c5a1c08cdd908a49f7cb73384b70b
Tomasz Cichoń <mrciastexx@gmail.com>
2017-06-24 10:59:02 +0200

    Changed the way system version is retrieved.

--- Spectrum.API/SystemVersion.cs
+++ Spectrum.API/SystemVersion.cs
@@ -2,12 +2,14 @@ namespace Spectrum.API
 {
     public class SystemVersion
     {
-        public static readonly APILevel APILevel = APILevel.UltraViolet;
+        public static readonly APILevel APILevel = APILevel.XRay;
 
         public static int DistanceBuild
         {
             get { return SVNRevision.number_; }
             set { SVNRevision.number_ = value; }
         }
+
+        public static string VersionString => $"DISTANCE {DistanceBuild} (SPECTRUM [00AA77]{APILevel.ToString()}[-])";
     }
 }
