ccac27c7b1f59766d85efd57f941a7175a6fd147
Nicolas <nicolas-donald.laurent@laposte.net>
2017-10-08 23:34:21 +0200

    First try

---
+++ Spectrum.Plugins.AutoServer/Entry.cs
@@ -0,0 +1,131 @@
+﻿using Spectrum.API;
+using Spectrum.API.Interfaces.Plugins;
+using Spectrum.API.Interfaces.Systems;
+using Spectrum.API.Game.Network;
+using System.Collections.Generic;
+using System;
+using Spectrum.API.Configuration;
+using System.Linq;
+using Events.MainMenu;
+using Events.Network;
+using Events.Player;
+using UnityEngine;
+using System.Collections;
+using Events;
+
+namespace Spectrum.Plugins.AutoServer
+{
+    public class Entry : IPlugin
+    {
+        public string FriendlyName => "Auto server";
+        public string Author => "Nico";
+        public string Contact => "SteamID: larnin";
+        public APILevel CompatibleAPILevel => APILevel.XRay;
+
+
+
+        public void Initialize(IManager manager)
+        {
+            Initialized.Subscribe(data =>
+            {
+                onMainMenuInitialized(data);
+            });
+            ServerInitialized.Subscribe(data =>
+            {
+                onServerInitialized(data);
+            });
+            
+            /*Server.Create("Test", "", 32);
+            G.Sys.GameManager_.GoToCurrentLevel();*/
+        }
+
+        void onMainMenuInitialized(Initialized.Data data)
+        {
+            G.Sys.GameManager_.StartCoroutine(startServerCoroutine());
+        }
+        
+        IEnumerator startServerCoroutine()
+        {
+            int state = 0;
+            while(true)
+            {
+                yield return new WaitForEndOfFrame();
+                switch(state)
+                {
+                    case 0:
+                        var obj1 = MonoBehaviour.FindObjectOfType<MainMenuLogic>();
+                        if(obj1 != null)
+                        {
+                            yield return new WaitForSeconds(0.1f);
+                            obj1.OnMultiplayerClicked();
+                            state++;
+                        }
+                        break;
+                    case 1:
+                        var obj2 = MonoBehaviour.FindObjectOfType<MainMenuLogic>();
+                        if (obj2 != null)
+                        {
+                            yield return new WaitForSeconds(0.1f);
+                            obj2.OnOnlineMPScreenClicked();
+                            state++;
+                        }
+                        break;
+                    case 2:
+                        var obj3 = MonoBehaviour.FindObjectOfType<OnlineMenuLogic>();
+                        if(obj3 != null)
+                        {
+                            yield return new WaitForSeconds(0.1f);
+                            obj3.OnHostAGameClicked();
+                            state++;
+                        }
+                        break;
+                    case 3:
+                        var obj4 = MonoBehaviour.FindObjectOfType<OnlineMenuLogic>();
+                        if(obj4 != null)
+                        {
+                            yield return new WaitForSeconds(0.1f);
+                            obj4.StartConnectionTimer();
+                            Server.Create("Test server", "", 32);
+                            state++;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                if (state > 3)
+                    break;
+                /*var obj = MonoBehaviour.FindObjectOfType<OnlineMenuLogic>();
+                if (obj == null)
+                    continue;
+
+                Console.WriteLine("OnlineMenuLogic found !");
+                obj.StartConnectionTimer();
+                G.Sys.NetworkingManager_.CreateServer("Test server", "", 32);
+                break;*/
+            }
+
+        }
+
+        void onServerInitialized(ServerInitialized.Data data)
+        {
+            G.Sys.GameManager_.StartCoroutine(cleanCoroutine());
+        }
+
+        IEnumerator cleanCoroutine()
+        {
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForEndOfFrame();
+            StaticEvent<ClientDisconnected.Data>.Broadcast(new ClientDisconnected.Data(Network.player, DisconnectionType.Quit));
+            Console.WriteLine("serverInitialized");
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForEndOfFrame();
+            G.Sys.GameManager_.GoToCurrentLevel();
+        }
+
+        public void Shutdown()
+        {
+            
+        }
+    }
+}
